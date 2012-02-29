fd16699045cfd71462171b4ed293515701f83182
Tyler Butler <tyler@tylerbutler.com>
2012-02-29 11:46:37 -0800

    Fix exception if site has no template pages.

--- sample_site_settings.py
+++
@@ -1,9 +0,0 @@
-# coding=utf-8
-
-# CUSTOMIZE EVERYTHING BELOW AS NEEDED
-SITE_TITLE = 'Tyler Butler'
-SITE_URL = 'http://tylerbutler.com'
-SITE_DESCRIPTION = 'tylerbutler.com'
-
-HOME_URL = '/'
-CONTENT_ROOT_DIR = 'sample_site'

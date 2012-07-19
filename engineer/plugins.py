aed8d3f648d7bbab413ecc71298c2abd393b5370
Tyler Butler <tyler@tylerbutler.com>
2012-07-18 18:00:40 -0700

    Fix major bug in post breaks processor.
    Previously the post breaks processor broke apart the Markdown into teaser
    content and full content, then converted each resulting piece to HTML
    independently. This meant that any Markdown links of the form
    [link][1] would fail to be converted properly to HTML links, since the
    teaser Markdown fragment didn't include the link references (they're
    at the bottom of the post).
    
    With this fix the break marker is first normalized to an HTML comment
    (the Octopress-style break marker) since we know that the Markdown
    processor will not touch it. Then the full post is converted to HTML
    and the result is split on the break marker. The teaser content is then
    stored in properly-converted HTML.

--- engineer/plugins.py
+++ engineer/plugins.py
@@ -112,12 +112,20 @@ class PostBreaksProcessor(PostProcessor):
     def preprocess(cls, post, metadata):
         from engineer.models import Post
 
+        # First check if either form of the break marker is present using the regex
         parsed_content = re.match(cls._regex, post.content_preprocessed)
         if parsed_content is None or parsed_content.group('teaser_content') is None:
             post.content_teaser = None
             return post
 
-        post.content_teaser = Post.convert_to_html(parsed_content.group('teaser_content'))
+        # Post is meant to be broken apart, so normalize the break marker to the HTML comment form.
         post.content_preprocessed = str(parsed_content.group('teaser_content') +
+                                        '<!-- more -->' +
                                         parsed_content.group('rest_of_content'))
+
+        # Convert the full post to HTML, then use the regex again to split the resulting HTML post. This is needed
+        # since Markdown might have links in the first half of the post that are listed at the bottom. By converting
+        # the whole post to HTML first then splitting we get a correctly processed HTML teaser.
+        parsed_content = re.match(cls._regex, Post.convert_to_html(post.content_preprocessed))
+        post.content_teaser = parsed_content.group('teaser_content')
         return post

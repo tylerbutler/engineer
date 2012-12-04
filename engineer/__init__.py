24412a0c31025ee986df0db1dea0d083575f43bc
Tyler Butler <tyler@tylerbutler.com>
2012-12-04 12:43:54 -0800

    Update automatic version handling.

--- engineer/__init__.py
+++ engineer/__init__.py
@@ -4,11 +4,13 @@ import os
 try:
     from _version import version
 except ImportError:
-
     print "Got an import error"
     from propane_distribution import update_version_py
 
     update_version_py(version_path=os.path.dirname(__file__))
-    from _version import version
+    try:
+        from engineer._version import version
+    except ImportError:
+        raise
 
 __author__ = 'Tyler Butler <tyler@tylerbutler.com>'
